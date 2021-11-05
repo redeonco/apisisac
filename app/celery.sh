d11f4be8eb08b58d75c7805f0e78b9dc9f22d5fa
tonycarvalho1994 <tony.carvalho1994@gmail.com>
2021-11-05 16:15:53 -0300

    celery no modo info

--- app/celery.sh
+++ app/celery.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-celery -A api worker -l error -B --scheduler django_celery_beat.schedulers:DatabaseScheduler &
+celery -A api worker -l info -B --scheduler django_celery_beat.schedulers:DatabaseScheduler &
 
