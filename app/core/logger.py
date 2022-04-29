4b6cb67c369ff7420c631101921c667df71b5feb
root <root@MAIMPTINOT002.oncoradium.corp>
2022-04-29 15:47:25 -0300

    filename comentado

--- app/core/logger.py
+++ app/core/logger.py
@@ -3,7 +3,8 @@ import logging
 LOGGER_LEVEL = logging.INFO
 LOGGER_NAME = 'API_LOGGER'
 
-logging.basicConfig(filename='api_log_file',
+logging.basicConfig(
+    # filename='api_log_file',
     filemode='a',
     format='[%(asctime)s] %(name)s - %(levelname)s: %(message)s',
     datefmt='%d/%m/%Y %H:%M:%S',
